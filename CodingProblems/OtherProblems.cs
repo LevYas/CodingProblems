3514cade3074968b4fb68c22c8eaa980adc5ebd8
Lev Yastrebov <levyas@outlook.com>
2019-08-23 18:35:01 +0300

    Added solution to the leetcode problem of selective reversing of a linked list. Runtime: 96 ms, faster than 69.01% of C# online submissions.

--- CodingProblems/OtherProblems.cs
+++ CodingProblems/OtherProblems.cs
@@ -19,4 +19,72 @@ namespace CodingProblems
                });
         }
     }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
+        public override string ToString()
+        {
+            return $"val={val}, next='{(next == null ? "null" : next.val.ToString())}'";
+        }
+    }
+
+    // Reverse a linked list from position m to n. Do it in one-pass.
+    // Note: 1 ≤ m ≤ n ≤ length of list.
+    // https://leetcode.com/problems/reverse-linked-list-ii/
+    public class LinkedListReverser
+    {
+        public static ListNode ReverseBetween(ListNode head, int begPos, int endPos)
+        {
+            ListNode prev  = null;
+            ListNode current = head;
+            int currentPos = 1;
+
+            ListNode beforeBegPosNode = null;
+            ListNode begPosNode = null;
+
+            ListNode endPosNode = null;
+            ListNode afterEndPosNode = null;
+
+            while(current != null)
+            {
+                if (currentPos == begPos - 1)
+                    beforeBegPosNode = current;
+
+                if (currentPos == begPos)
+                    begPosNode = current;
+
+                if (currentPos == endPos)
+                    endPosNode = current;
+
+                if (currentPos == endPos + 1)
+                {
+                    afterEndPosNode = current;
+                    break;
+                }
+
+                ListNode next = current.next;
+
+                if (currentPos > begPos && currentPos <= endPos)
+                    current.next = prev;
+
+                prev = current;
+                current = next;
+
+                ++currentPos;
+            }
+
+            if (beforeBegPosNode != null)
+                beforeBegPosNode.next = endPosNode;
+            else
+                head = endPosNode;
+
+            begPosNode.next = afterEndPosNode;
+
+            return head;
+        }
+    }
 }
--- CodingProblemsTests/OtherProblemsTests.cs
+++ CodingProblemsTests/OtherProblemsTests.cs
@@ -16,5 +16,51 @@ namespace CodingProblemsTests
             {
                 { new[] { (30, 75), (0, 50), (60, 150) }, 2 }
             };
+
+        [Theory]
+        [MemberData(nameof(ReverseBetweenTestData))]
+        public void ReverseBetweenTest(ListNode sourseList, int beg, int end, int[] expectedValues)
+        {
+            ListNode resultingList = LinkedListReverser.ReverseBetween(sourseList, beg, end);
+            ListNode currentNode = resultingList;
+
+            foreach (int expectedVal in expectedValues)
+            {
+                Assert.Equal(expectedVal, currentNode.val);
+                currentNode = currentNode.next;
+            }
+        }
+
+        public static object[][] ReverseBetweenTestData()
+        {
+            return new object[][]
+            {
+                new object[] { createFilledLinkedList(1), 1, 1, new[] { 1 } },
+
+                new object[] { createFilledLinkedList(2), 1, 1, new[] { 1, 2 } },
+                new object[] { createFilledLinkedList(2), 2, 2, new[] { 1, 2 } },
+                new object[] { createFilledLinkedList(2), 1, 2, new[] { 2, 1 } },
+
+                new object[] { createFilledLinkedList(5), 3, 3, new[] { 1, 2, 3, 4, 5 } },
+                new object[] { createFilledLinkedList(5), 2, 4, new[] { 1, 4, 3, 2, 5 } },
+                new object[] { createFilledLinkedList(5), 1, 5, new[] { 5, 4, 3, 2, 1 } },
+            };
+        }
+
+        private static ListNode createFilledLinkedList(int length)
+        {
+            if (length < 1)
+                return null;
+
+            ListNode head = new ListNode(1);
+            ListNode last = head;
+            for (int i = 2; i <= length; i++)
+            {
+                last.next = new ListNode(i);
+                last = last.next;
+            }
+
+            return head;
+        }
     }
 }
