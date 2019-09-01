740f409487568843aaf9f650058cc98a6ef09327
Lev Yastrebov <levyas@outlook.com>
2019-09-01 22:41:01 +0300

    Added solution to the n-queens puzzle problem using backtracking. Runtime: 264 ms, faster than 84.41% of C# online submissions. Memory Usage: 31.4 MB, less than 100.00% of C# online submissions.

--- CodingProblemsTests/OtherProblemsTests.cs
+++ CodingProblemsTests/OtherProblemsTests.cs
@@ -1,4 +1,5 @@
 ﻿using CodingProblems;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CodingProblemsTests
@@ -16,6 +17,50 @@ namespace CodingProblemsTests
             {
                 { new[] { (30, 75), (0, 50), (60, 150) }, 2 }
             };
+
+        [Theory]
+        [MemberData(nameof(SolveNQueensTestData))]
+        public void SolveNQueensTest(int numberOfQueensAndSize, int solutionsAmount, IList<IList<string>> expectedBoards = null)
+        {
+            var result = OtherProblems.SolveNQueens(numberOfQueensAndSize);
+
+            Assert.Equal(solutionsAmount, result.Count);
+
+            if (expectedBoards == null)
+                return;
+            
+            for (int resultIdx = 0; resultIdx < expectedBoards.Count; resultIdx++)
+            {
+                for (int rowIdx = 0; rowIdx < expectedBoards[resultIdx].Count; rowIdx++)
+                    Assert.Equal(expectedBoards[resultIdx][rowIdx], result[resultIdx][rowIdx]);
+            }
+        }
+
+        public static object[][] SolveNQueensTestData()
+        {
+            return new object[][]
+            {
+                new object[] { 0, 1 },
+                new object[] { 1, 1, new List<IList<string>>() { new List<string>() { "Q" } } },
+                new object[] { 2, 0 },
+                new object[] { 3, 0 },
+
+                new object[] { 4, 2, new List<IList<string>>()
+                    {
+                        new List<string>() { ".Q..", "...Q", "Q...", "..Q." },
+                        new List<string>() { "..Q.", "Q...", "...Q", ".Q.." }
+                    },
+                },
+
+                // Others are from: https://oeis.org/A000170
+                new object[] { 5, 10 },
+                new object[] { 6, 4 },
+                new object[] { 7, 40 },
+                new object[] { 8, 92 },
+                new object[] { 9, 352 },
+                new object[] { 10, 724 },
+            };
+        }
 
         [Theory]
         [MemberData(nameof(ReverseBetweenTestData))]
