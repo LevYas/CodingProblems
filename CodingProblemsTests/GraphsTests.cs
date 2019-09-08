47dd0444b84b95ddfbd94e9bc7bf0d7dc159a3b8
Lev Yastrebov <levyas@outlook.com>
2019-09-08 15:04:35 +0300

    N-queens solver moved to more appropriate category - graphs.

--- CodingProblemsTests/GraphsTests.cs
+++ CodingProblemsTests/GraphsTests.cs
@@ -110,4 +110,51 @@ namespace CodingProblemsTests
             return arrs.ToArray();
         }
     }
+
+    public class NQueensSolverTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void SolveNQueensTest(int numberOfQueensAndSize, int solutionsAmount, IList<IList<string>> expectedBoards = null)
+        {
+            var result = NQueensSolver.SolveNQueens(numberOfQueensAndSize);
+
+            Assert.Equal(solutionsAmount, result.Count);
+
+            if (expectedBoards == null)
+                return;
+
+            for (int resultIdx = 0; resultIdx < expectedBoards.Count; resultIdx++)
+            {
+                for (int rowIdx = 0; rowIdx < expectedBoards[resultIdx].Count; rowIdx++)
+                    Assert.Equal(expectedBoards[resultIdx][rowIdx], result[resultIdx][rowIdx]);
+            }
+        }
+
+        public static object[][] TestData()
+        {
+            return new object[][]
+            {
+                new object[] { 0, 1 },
+                new object[] { 1, 1, new List<IList<string>>() { new List<string>() { "Q" } } },
+                new object[] { 2, 0 },
+                new object[] { 3, 0 },
+
+                new object[] { 4, 2, new List<IList<string>>()
+                    {
+                        new List<string>() { ".Q..", "...Q", "Q...", "..Q." },
+                        new List<string>() { "..Q.", "Q...", "...Q", ".Q.." }
+                    },
+                },
+
+                // Others are from: https://oeis.org/A000170
+                new object[] { 5, 10 },
+                new object[] { 6, 4 },
+                new object[] { 7, 40 },
+                new object[] { 8, 92 },
+                new object[] { 9, 352 },
+                new object[] { 10, 724 },
+            };
+        }
+    }
 }
--- CodingProblemsTests/OtherProblemsTests.cs
+++ CodingProblemsTests/OtherProblemsTests.cs
@@ -19,50 +19,6 @@ namespace CodingProblemsTests
             };
 
         [Theory]
-        [MemberData(nameof(SolveNQueensTestData))]
-        public void SolveNQueensTest(int numberOfQueensAndSize, int solutionsAmount, IList<IList<string>> expectedBoards = null)
-        {
-            var result = OtherProblems.SolveNQueens(numberOfQueensAndSize);
-
-            Assert.Equal(solutionsAmount, result.Count);
-
-            if (expectedBoards == null)
-                return;
-            
-            for (int resultIdx = 0; resultIdx < expectedBoards.Count; resultIdx++)
-            {
-                for (int rowIdx = 0; rowIdx < expectedBoards[resultIdx].Count; rowIdx++)
-                    Assert.Equal(expectedBoards[resultIdx][rowIdx], result[resultIdx][rowIdx]);
-            }
-        }
-
-        public static object[][] SolveNQueensTestData()
-        {
-            return new object[][]
-            {
-                new object[] { 0, 1 },
-                new object[] { 1, 1, new List<IList<string>>() { new List<string>() { "Q" } } },
-                new object[] { 2, 0 },
-                new object[] { 3, 0 },
-
-                new object[] { 4, 2, new List<IList<string>>()
-                    {
-                        new List<string>() { ".Q..", "...Q", "Q...", "..Q." },
-                        new List<string>() { "..Q.", "Q...", "...Q", ".Q.." }
-                    },
-                },
-
-                // Others are from: https://oeis.org/A000170
-                new object[] { 5, 10 },
-                new object[] { 6, 4 },
-                new object[] { 7, 40 },
-                new object[] { 8, 92 },
-                new object[] { 9, 352 },
-                new object[] { 10, 724 },
-            };
-        }
-
-        [Theory]
         [MemberData(nameof(ReverseBetweenTestData))]
         public void ReverseBetweenTest(ListNode sourseList, int beg, int end, int[] expectedValues)
         {
