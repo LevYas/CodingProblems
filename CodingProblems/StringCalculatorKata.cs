dbf2ca5bc38a98cc3704cb3ca5e2cf8e6e55d117
Lev Yastrebov <levyas@outlook.com>
2019-12-23 23:16:55 +0300

    Added my implementation of String Calculator Kata - a special kind of tasks, where you don't know what requirement will be the next and you have only 30 minutes to complete all the steps. Kata source: https://osherove.com/tdd-kata-1/

---
+++ CodingProblems/StringCalculatorKata.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingProblems
+{
+    // String Calculator Kata
+    // This is a special kind of tasks, where you don't know what requirement will be the next
+    // and you have only 30 minutes to complete all the steps.
+    // Steps and instructions are here:
+    // https://github.com/ardalis/kata-catalog/blob/master/katas/String%20Calculator.md or https://osherove.com/tdd-kata-1/
+    // So, this code was written using TDD and in only 30 minutes from scratch (not on the first try, of course)
+    public static class StringCalculator
+    {
+        public static int Add(string numSequence)
+        {
+            if (String.IsNullOrEmpty(numSequence))
+                return 0;
+
+            var delimiters = new List<string> { ",", "\n" };
+
+            if (numSequence.StartsWith("//"))
+            {
+                if (numSequence[2] == '[')
+                {
+                    numSequence = numSequence.Substring(2);
+
+                    while (numSequence.IndexOf(']') != -1)
+                    {
+                        numSequence = numSequence.Substring(1);
+
+                        int closingBracketIdx = numSequence.IndexOf(']');
+                        delimiters.Add(numSequence.Substring(0, closingBracketIdx));
+
+                        numSequence = numSequence.Substring(closingBracketIdx + 1);
+                    }
+
+                    numSequence = numSequence.Substring(1);
+                }
+                else
+                {
+                    delimiters.Add(numSequence[2].ToString());
+                    numSequence = numSequence.Substring(4);
+                }
+            }
+
+            IEnumerable<int> nums = numSequence.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+
+            IEnumerable<int> negatives = nums.Where(n => n < 0);
+            if (negatives.Any())
+                throw new ArgumentException($"Negatives not allowed: {String.Join(",", negatives)}");
+
+            return nums.Where(n => n < 1000).Sum();
+        }
+    }
+}
---
+++ CodingProblemsTests/StringCalculatorKataTests.cs
@@ -0,0 +1,70 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class StringCalculatorTests
+    {
+        private const string _expectedMessageOnNegativesException = "Negatives not allowed: ";
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("3", 3)]
+        [InlineData("5,6", 11)]
+        [InlineData("5,6,0,5", 16)]
+        [InlineData("5\n6\n0,5", 16)]
+        [InlineData("//;\n5;6;0;5", 16)]
+        public void AddAdds(string numSeq, int expected)
+        {
+            Assert.Equal(expected, StringCalculator.Add(numSeq));
+        }
+
+        [Fact]
+        public void AddThrowsOnNegatives()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => StringCalculator.Add("-3"));
+            Assert.StartsWith(_expectedMessageOnNegativesException, ex.Message);
+        }
+
+        [Theory]
+        [InlineData("-3", new int[] { -3 })]
+        [InlineData("5,-6", new int[] { -6 })]
+        [InlineData("5,-6,0,-5", new int[] { -6, -5 })]
+        [InlineData("//;\n-5;-6;0;5", new int[] { -5, -6 })]
+        public void AddReportsNegatives(string numSeq, int[] expected)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => StringCalculator.Add(numSeq));
+
+            IEnumerable<int> reportedNumbers = ex.Message.Substring(_expectedMessageOnNegativesException.Length).Split(',').Select(Int32.Parse);
+            expected.Should().BeEquivalentTo(reportedNumbers);
+        }
+
+        [Theory]
+        [InlineData("3,1001", 3)]
+        [InlineData("5,6,0,1001", 11)]
+        [InlineData("//;\n5;6;0;5;10100", 16)]
+        public void AddIgnoresValuesMoreThan1000(string numSeq, int expected)
+        {
+            Assert.Equal(expected, StringCalculator.Add(numSeq));
+        }
+
+        [Theory]
+        [InlineData("//[|||]\n1|||2|||3", 6)]
+        public void AddAcceptsLongDelimiters(string numSeq, int expected)
+        {
+            Assert.Equal(expected, StringCalculator.Add(numSeq));
+        }
+
+        [Theory]
+        [InlineData("//[|][%]\n1|2%3", 6)]
+        [InlineData("//[|i][%i]\n1|i2%i3", 6)]
+        public void AddAcceptsMultipleDelimiters(string numSeq, int expected)
+        {
+            Assert.Equal(expected, StringCalculator.Add(numSeq));
+        }
+    }
+}
