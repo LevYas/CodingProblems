91c7da1738a3ec0bdc46a3ab2a03bf7f697bf6b3
Lev Yastrebov <levyas@outlook.com>
2020-01-02 15:43:31 +0300

    Added solution to "Roman to Integer" problem. Runtime: 84 ms, faster than 90.16% of C# online submissions.

---
+++ CodingProblems/Numbers/RomanToIntegerConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodingProblems.Numbers
+{
+    public static class RomanToIntegerConverter
+    {
+        // Roman to Integer
+        // Given a roman numeral, convert it to an integer.
+        // https://leetcode.com/problems/roman-to-integer/
+        public static int RomanToInt(string s)
+        {
+            int result = 0;
+            int prevVal = -1;
+
+            for (int i = s.Length - 1; i >= 0 ; i--)
+            {
+                int curVal = getSymbolValue(s[i]);
+
+                if (result == 0 || curVal >= prevVal)
+                    result += curVal;
+                else
+                    result -= curVal;
+
+                prevVal = curVal;
+            }
+
+            return result;
+        }
+
+        private static int getSymbolValue(char s)
+        {
+            switch (s)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: throw new Exception();
+            }
+        }
+    }
+}
---
+++ CodingProblemsTests/NumbersTests/RomanToIntegerConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using CodingProblems.Numbers;
+using Xunit;
+
+namespace CodingProblemsTests.NumbersTests
+{
+    public class RomanToIntegerConverterTests
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("V", 5)]
+        [InlineData("M", 1000)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void ProducesTheRightResult(string input, int expected)
+        {
+            Assert.Equal(expected, RomanToIntegerConverter.RomanToInt(input));
+        }
+    }
+}
