668ed066ec8b7d5dd8427409da5a130e0bf9c4c8
Lev Yastrebov <levyas@outlook.com>
2020-01-02 22:00:08 +0300

    Occasionally found "Set to power set" problem as "Subsets" problem on leetcode. Runtime: 236 ms, faster than 97.51% of C# online submissions

--- CodingProblems/MathProblems.cs
+++ CodingProblems/MathProblems.cs
@@ -39,13 +39,14 @@ namespace CodingProblems
             return area / (radius * radius);
         }
 
-        // Daily Coding Problem: Problem #37
+        // Subsets (Daily Coding Problem: Problem #37)
         // The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
         // For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
         // You may also use a list or array to represent a set.
-        public static IEnumerable<IEnumerable<int>> SetToPowerSet(IEnumerable<int> set)
+        // https://leetcode.com/problems/subsets/
+        public static IList<IList<int>> SetToPowerSet(IEnumerable<int> set)
         {
-            List<List<int>> powerSet = new List<List<int>>();
+            List<IList<int>> powerSet = new List<IList<int>>();
 
             foreach (int elem in set)
             {
