84a668814c30bdf06a4126aa11d921040c3e5f34
Lev Yastrebov <levyas@outlook.com>
2020-01-02 21:25:40 +0300

    Added solution to "Add Binary" problem. Runtime: 84 ms, faster than 87.96% of C# online submissions.

---
+++ CodingProblems/Numbers/BinaryOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodingProblems.Numbers
+{
+    public static class BinaryOperations
+    {
+        // Add Binary
+        // Given two binary strings, return their sum (also a binary string).
+        // https://leetcode.com/problems/add-binary/
+        public static string AddBinary(string num1, string num2)
+        {
+            string result = "";
+
+            int curIdx1 = num1.Length - 1;
+            int curIdx2 = num2.Length - 1;
+
+            string bit;
+            int carry = 0;
+            int itemFrom1, itemFrom2;
+
+            while (curIdx1 >= 0 || curIdx2 >= 0 || carry != 0)
+            {
+                itemFrom1 = curIdx1 >= 0 ? num1[curIdx1--] - '0' : 0;
+                itemFrom2 = curIdx2 >= 0 ? num2[curIdx2--] - '0' : 0;
+
+                (bit, carry) = findBitAndCarry(carry + itemFrom1 + itemFrom2);
+                result = bit + result;
+            }
+
+            return result;
+        }
+
+        private static (string, int) findBitAndCarry(int sum)
+        {
+            switch (sum)
+            {
+                case 0: return ("0", 0);
+                case 1: return ("1", 0);
+                case 2: return ("0", 1);
+                case 3: return ("1", 1);
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ CodingProblemsTests/NumbersTests/BinaryOperationsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodingProblems.Numbers;
+using Xunit;
+
+namespace CodingProblemsTests.NumbersTests
+{
+    public class BinaryOperationsTests
+    {
+        [Theory]
+        [InlineData("1", "1", "10")]
+        [InlineData("11", "1", "100")]
+        [InlineData("1010", "1011", "10101")]
+        public void AddsBinaryNumbers(string str1, string str2, string expected)
+        {
+            Assert.Equal(expected, BinaryOperations.AddBinary(str1, str2));
+        }
+    }
+}
