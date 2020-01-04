a46608b3af19c519ef70345bad18c742367f7439
Lev Yastrebov <levyas@outlook.com>
2020-01-04 19:14:24 +0300

    Added solution to "Valid Parentheses" leetcode problem. Runtime: 72 ms, faster than 86.19% of C# online submissions.

---
+++ CodingProblems/Other/ValidParenthesesChecker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingProblems.Other
+{
+    // Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    // An input string is valid if:
+    //      Open brackets must be closed by the same type of brackets.
+    //      Open brackets must be closed in the correct order.
+    // Note that an empty string is also considered valid.
+    // https://leetcode.com/problems/valid-parentheses/
+    public static class ValidParenthesesChecker
+    {
+        public static bool IsValid(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return true;
+
+            Stack<char> parentheses = new Stack<char>(str.Length / 2);
+
+            foreach (char parenthesis in str)
+            {
+                if (parenthesis == '(' || parenthesis == '[' || parenthesis == '{')
+                {
+                    parentheses.Push(parenthesis);
+                    continue;
+                }
+
+                if (parentheses.Count == 0 || !areMatching(parentheses.Pop(), parenthesis))
+                    return false;
+            }
+
+            return parentheses.Count == 0;
+        }
+
+        private static bool areMatching(char opening, char closing)
+        {
+            switch (closing)
+            {
+                case ')': return opening == '(';
+                case ']': return opening == '[';
+                case '}': return opening == '{';
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ CodingProblemsTests/OtherTests/ValidParenthesesCheckerTests.cs
@@ -0,0 +1,21 @@
+﻿using CodingProblems.Other;
+using Xunit;
+
+namespace CodingProblemsTests.OtherTests
+{
+    public class ValidParenthesesCheckerTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("{[]}", true)]
+        [InlineData("([)]", false)]
+        [InlineData("]", false)]
+        public void ChecksCorrectly(string input, bool expected)
+        {
+            Assert.Equal(expected, ValidParenthesesChecker.IsValid(input));
+        }
+    }
+}
