c06748188f62d179e7858d6ffe05b26edf08ad56
Lev Yastrebov <levyas@outlook.com>
2020-10-21 13:24:24 +0300

    Added solution to the "Cinema Seat Allocation" LeetCode problem. Runtime: 268 ms, faster than 94.44% of submissions.

---
+++ CodingProblems/Other/CinemaSeatAllocator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingProblems.Other
+{
+    // A cinema has n rows of seats, numbered from 1 to n and there are ten seats in each row, labeled from 1 to 10.
+    // Given the array reservedSeats containing the numbers of seats already reserved, for example,
+    //      reservedSeats[i] = [3,8] means the seat located in row 3 and labeled with 8 is already reserved.
+    // Return the maximum number of four-person groups you can assign on the cinema seats.
+    // A four-person group occupies four adjacent seats in one single row.
+    // Seats across an aisle (such as [3,3] and [3,4]) are not considered to be adjacent,
+    // but there is an exceptional case on which an aisle split a four-person group, in that case,
+    // the aisle split a four-person group in the middle, which means to have two people on each side.
+    // https://leetcode.com/problems/cinema-seat-allocation/
+    public static class CinemaSeatAllocator
+    {
+        public static int MaxNumberOfFamilies(int numberOfRows, int[][] reservedSeats)
+        {
+            // We use integer in value as mask. It's not kind of a real world solution,
+            // but LeetCode wants to process thousand values very quickly
+            var reservations = new Dictionary<int, int>();
+
+            // we are not interested in end seats
+            foreach (int[] reservedSeat in reservedSeats.Where(c => c[1] > 1 && c[1] < 10))
+            {
+                if (reservations.TryGetValue(reservedSeat[0], out int rowMask))
+                    reservations[reservedSeat[0]] = rowMask | 1 << reservedSeat[1] - 2;
+                else
+                    reservations.Add(reservedSeat[0], 1 << reservedSeat[1] - 2);
+            }
+
+            int possiblePlacementsAmount = numberOfRows * 2;
+
+            foreach (int rowMask in reservations.Values)
+            {
+                bool areLeftSeatsFree = (rowMask & 0b1111) == 0;
+                bool areCenterSeatsFree = (rowMask & 0b111100) == 0;
+                bool areRightSeatsFree = (rowMask & 0b11110000) == 0;
+
+                if (areLeftSeatsFree || areCenterSeatsFree || areRightSeatsFree)
+                    possiblePlacementsAmount--;
+                else
+                    possiblePlacementsAmount -= 2;
+            }
+
+            return possiblePlacementsAmount;
+        }
+    }
+}
---
+++ CodingProblemsTests/Other/CinemaSeatAllocatorTests.cs
@@ -0,0 +1,57 @@
+using CodingProblems.Other;
+using System;
+using System.Collections.Generic;
+using CodingProblemsTests.Utility;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests.Other
+{
+    public class CinemaSeatAllocatorTests
+    {
+        [Theory]
+        [MemberData(nameof(CinemaSeatAllocatorTestData))]
+        public void SolutionWorks(int numberOfRows, int[][] reservedSeats, int expected)
+        {
+            CinemaSeatAllocator.MaxNumberOfFamilies(numberOfRows, reservedSeats).Should().Be(expected);
+        }
+
+        public static TheoryData<int, int[][], int> CinemaSeatAllocatorTestData
+            => new TheoryData<int, int[][], int>
+            {
+                { 2, "[1,1],[2,6],[1,3]".ToArrays(), 2 },
+                { 1, Array.Empty<int[]>(), 2 },
+                { 1, "[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9]".ToArrays(), 0 },
+                { 1, "[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]".ToArrays(), 0 },
+                { 1, "[1,2],[1,3],[1,4],[1,5],[1,6],[1,7]".ToArrays(), 0 },
+                { 1, "[1,2],[1,3],[1,4],[1,5],[1,6]".ToArrays(), 0 },
+                { 1, "[1,2],[1,3],[1,4],[1,5]".ToArrays(), 1 },
+                { 1, "[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9]".ToArrays(), 0 },
+                { 1, "[1,4],[1,5],[1,6],[1,7],[1,8],[1,9]".ToArrays(), 0 },
+                { 1, "[1,5],[1,6],[1,7],[1,8],[1,9]".ToArrays(), 0 },
+                { 1, "[1,6],[1,7],[1,8],[1,9]".ToArrays(), 1 },
+                { 1, "[1,2],[1,3],[1,8],[1,9]".ToArrays(), 1 },
+                { 1, "[1,2],[1,9]".ToArrays(), 1 },
+                { 1, "[1,3],[1,8]".ToArrays(), 1 },
+                { 11, "[11,2],[11,8]".ToArrays(), 21 },
+            };
+
+        [Fact]
+        public void SolutionWorksTillTheLimits()
+        {
+            const int numberOfRows = 1000_000_000;
+            var seats = new List<int[]>();
+
+            for (int i = 1000000; i < numberOfRows; i++)
+            {
+                if (seats.Count >= 1000)
+                    break;
+
+                for (int j = 1; j <= 10; j++)
+                    seats.Add(new[] {i + 1, j});
+            }
+
+            CinemaSeatAllocator.MaxNumberOfFamilies(numberOfRows, seats.ToArray()).Should().BePositive();
+        }
+    }
+}
